3c4ace09c3adf59ecee3f4aa1575ff526e8220a3
fb0801 <fbhatti0801@gmail.com>
2023-04-14 17:55:03 +0100

    lets build 5

--- minesweeper.js
+++ minesweeper.js
@@ -9,6 +9,8 @@ const TILE_STATUSES ={
 
 export function createBoard(boardSize, numberOfMines){
     const board = []
+    const minPosition = getMinePositions(boardSize, numberOfMines)
+
     for (let x = 0; x < boardSize; x++) {
         const row = []
         for(let y=0; y < boardSize; x++) {
@@ -18,7 +20,7 @@ export function createBoard(boardSize, numberOfMines){
                 element,
                 x,
                 y, 
-                mine: True
+                mine: minPosition.some(),
                 get status(){
                     return this.element.dataset.status
                 },
@@ -31,4 +33,27 @@ export function createBoard(boardSize, numberOfMines){
         board.push(row)
     }
     return board
+}
+
+function getMinePositions(boardSize, numberOfMines){
+    const positions = []
+    
+    while(positions.length< numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize)
+        }
+        if (!positions.some( positionMatch.bind(null, position))) {
+            positions.push(position)
+        }
+    }
+    return positions
+}
+
+function positionMatch(a,b){
+    return a.x === b.x && a.y === b.y 
+}
+
+function randomNumber(size){
+    return Math.floor(Math.random() * size)
 }
